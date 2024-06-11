b0dbb7a1c690b9a90e2fc638ed134095c033ec59
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-06-10 19:48:47 -0500

    form and js finished

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const form = document.getElementById("infoForm");
 
-
-function veryifyEmail(email1, email2) {
-
-}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let email1 = document.getElementById("email");
+    let email2 = document.getElementById("verify-email");
+    if(email1.value != email2.value) {
+        alert("make sure your emails match")
+    }
+});
